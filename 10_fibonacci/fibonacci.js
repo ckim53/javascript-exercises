20da73a67c726875b7d0a8a5826d24576c6112c0
Christina <184446177+ckim53@users.noreply.github.com>
2024-12-02 14:51:46 -0800

    Complete exercises 1-10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num == 0){
+        return 0;
+    }
+    if (num < 0){
+        return "OOPS";
+    }
+    
+    let count = 0;
+    let current = 1, next = 1, prev = 1; 
+    let array = [1, 1];
 
+    while (array.length < num){
+        next = current + prev;
+        console.log(`next: ${next}`);
+        array.push(next);
+        prev = current;
+        current = next;
+        
+        console.log(array);
+        count += 1;
+    }
+    return array[count+1];
 };
 
 // Do not edit below this line
