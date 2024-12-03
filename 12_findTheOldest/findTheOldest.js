336b3ed79c2498978fbf0c07f81c553fb961deb2
Christina <184446177+ckim53@users.noreply.github.com>
2024-12-02 19:09:22 -0800

    Implement findTheOldest()

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let maxAge = 0, age = 0;
+    let oldest = '';
+    for (person of people) {
+        if (person.yearOfDeath == undefined){
+            let currentYear = new Date().getFullYear();
+            age = currentYear - person.yearOfBirth;
+        } else {
+            age = person.yearOfDeath - person.yearOfBirth;
+        }
+        console.log(`age: ${age}`);
+        if (age > maxAge) {
+            maxAge = age;
+            console.log(`max age: ${maxAge}`);
+            oldest = person;
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
