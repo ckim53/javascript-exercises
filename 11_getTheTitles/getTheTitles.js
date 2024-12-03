c9b72ee5b6384c891a9a90e7af8bfeb1da0729f3
Christina <184446177+ckim53@users.noreply.github.com>
2024-12-02 16:31:31 -0800

    Implement getTheTitles()

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let array = [];
+    for (book of books) {
+        array.push(book.title);
+    }
+    return array;
 };
 
 // Do not edit below this line
